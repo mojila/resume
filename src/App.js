b3aaaadae4c5b8a7bc2bb2ee2bba46f43baa416f
mojila <mojila@github.com>
2019-06-16 17:34:24 +0700

    separate menu component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,7 @@ import React from 'react';
 import ellipse from './images/ellipse.svg'
 import superhero from './images/superhero.svg'
 import GitHubButton from 'react-github-btn'
-
-const Menu = () => {
-  return (
-    <div className="pl-4 pt-2 pb-1 pr-4 d-flex justify-content-between roboto">
-      <div className="font-weight-bold primary font-size-25 pointer">mojila</div>
-      <div className="font-size-16 d-flex justify-content-start pt-1 primary">
-        <div className="menu-active mr-5 pointer">Home</div>
-        <div className="mr-5 pointer">Experiences</div>
-        <div className="mr-5 pointer">Services</div>
-        <div className="mr-5 pointer">Contact</div>
-        <div className="mr-5 pointer">GitHub</div>
-      </div>
-    </div>
-  )
-} 
+import Menu from './components/Menu';
 
 const App = () => {
   return (
---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div className="pl-4 pt-2 pb-1 pr-4 d-flex justify-content-between roboto">
+        <div className="font-weight-bold primary font-size-25 pointer">mojila</div>
+        <div className="font-size-16 d-flex justify-content-start pt-1 primary">
+            <div className="menu-active mr-5 pointer">Home</div>
+            <div className="mr-5 pointer">Experiences</div>
+            <div className="mr-5 pointer">Services</div>
+            <div className="mr-5 pointer">Contact</div>
+            <div className="mr-5 pointer">GitHub</div>
+        </div>
+        </div>
+    )
+} 
+
+export default Menu
